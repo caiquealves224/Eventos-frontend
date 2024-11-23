99e7fa42cc2c9bab029851079ef786cb73cde6f7
caique <caiquealves224@gmail.com>
2024-11-23 11:33:13 -0300

    feat: create AcessarViaQrCode component

--- src/components/evento/DashboardEvento.tsx
+++ src/components/evento/DashboardEvento.tsx
@@ -1,5 +1,6 @@
 import { Evento } from "@/core"
 import InformacoesEvento from "./InformacoesEvento"
+import AcessarViaQrCode from "./AcessarViaQrCode"
 
 export interface DashboardEventoProps {
     evento: Evento
@@ -7,10 +8,10 @@ export interface DashboardEventoProps {
 
 export default function DashboardEvento(props: DashboardEventoProps) {
     return (
-        <div>
-            <div>
-                <InformacoesEvento evento={props.evento} className="
-                flex-1"/>
+        <div className="flex flex-col gap-2">
+            <div className="flex gap-2 self-stretch">
+                <InformacoesEvento evento={props.evento} className="flex-1" />
+                <AcessarViaQrCode evento={props.evento} />
             </div>
         </div>
     )
--- src/components/evento/InformacoesEvento.tsx
+++ src/components/evento/InformacoesEvento.tsx
@@ -9,8 +9,13 @@ export interface InformacoesEventoProps {
 export default function InformacoesEvento(props: InformacoesEventoProps) {
     const { evento } = props;
     return (
-        <div>
-            <div className={`flex flex-col gap-2 ${props.className ?? ""}`}>
+        <div className={`flex flex-col gap-2 ${props.className ?? ""}`}>
+            <div className="flex-1 flex items-center gap-4 border border-zinc-800 px-6 py-3 rounded-lg">
+                <span className="text-2xl font-black">{evento.alias}</span>
+                <span className="text-xl text-zinc-300">{evento.nome}</span>
+            </div>
+
+            <div className="flex gap-2">
                 <Informacao label="Data:">
                     <span>
                         {new Date(evento.data!).toLocaleDateString()}
@@ -18,7 +23,13 @@ export default function InformacoesEvento(props: InformacoesEventoProps) {
                         {new Date(evento.data!).toLocaleTimeString()}
                     </span>
                 </Informacao>
+                <Informacao label="Local: ">
+                    {evento.local}
+                </Informacao>
             </div>
+            <Informacao label="Descrição">
+                {evento.descricao}
+            </Informacao>
         </div>
     )
 }
