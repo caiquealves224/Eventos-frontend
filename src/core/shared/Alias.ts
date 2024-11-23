f8b7ec9fa6f6f9ca8fd5d8a74b5aa915b9879353
caique <caiquealves224@gmail.com>
2024-11-23 20:33:00 -0300

    feat: update dependencies

--- src/core/shared/Alias.ts
+++
@@ -1,5 +0,0 @@
-export default class Alias {
-    static formatar(value: string): string {
-        return value.replace(/ /g, "-").toLowerCase()
-    }
-}
--- src/core/shared/index.ts
+++
@@ -1,6 +0,0 @@
-import Alias from "./Alias";
-import Id from "./Id";
-import Senha from "./Senha";
-
-
-export { Id, Alias, Senha }
