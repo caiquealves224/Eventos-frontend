ca77273604634db712a89729e9d3d45f2c9cdd8a
caique <caiquealves224@gmail.com>
2024-11-23 09:40:06 -0300

    feat: add qrcode

--- src/app/(temp)/eventos/page.tsx
+++ src/app/(temp)/eventos/page.tsx
@@ -1,6 +1,7 @@
 import { eventos } from "@/core";
 import Image from "next/image";
 import Link from "next/link";
+import QRCode from "react-qr-code";
 
 export default function PaginaEventos() {
     return (
@@ -22,9 +23,15 @@ export default function PaginaEventos() {
                         />
                     </div>
 
-                    <div className="flex-1 flex flex-col p-7 gap-5">
+                    <div className="flex-1 flex flex-col items-center p-7 gap-5">
                         <span className="text-lg font-balck">{evento.nome}</span>
-                        <p className="flex-1 text-sm text-zinc-400">{evento.descricao}</p>
+                        <p className="flex-1 text-sm text-zinc-400 text-center">{evento.descricao}</p>
+
+                        <QRCode
+                            value={JSON.stringify({id: evento.id, senha: evento.senha})}
+                            className="w-44 h-44"
+                        />
+
                         <div className="flex gap-5">
                             <Link className="flex-1 botao vermelho" href={`/evento/admin/${evento.id}/${evento.senha}`}>Admin</Link>
                             <Link className="flex-1 botao verde" href={`/convite//${evento.alias}`}>Convite</Link>
