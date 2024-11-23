83f38728c1815293f0bba6acc2c5cbf4f8119537
caique <caiquealves224@gmail.com>
2024-11-23 09:08:52 -0300

    feat: add config for images

--- src/app/(temp)/eventos/page.tsx
+++ src/app/(temp)/eventos/page.tsx
@@ -1,10 +1,28 @@
 import { eventos } from "@/core";
+import Image from "next/image";
 
 export default function PaginaEventos() {
     return (
         <div className="flex flex-col gap-5">
             {eventos.map(evento => (
-                <span key={evento.id}>{evento.nome}</span>
+                <div key={evento.id} className="
+                    flex-col flex-col w-96
+                    bg-zinc-800 rounded-lg
+                ">
+
+                    <div className="
+                        relative w-96 h-44
+                    ">
+                        <Image 
+                            src={evento.imagem}
+                            fill
+                            alt={evento.nome}
+                        />
+
+                    </div>
+
+                    <span>{evento.nome}</span>
+                </div>
             ))}
         </div>
     )
