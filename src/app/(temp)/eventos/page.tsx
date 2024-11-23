bdf790e5868be41e4eabc4b9d8da9b33dfdf8a9f
caique <caiquealves224@gmail.com>
2024-11-23 09:27:50 -0300

    feat: add buttons

--- src/app/(temp)/eventos/page.tsx
+++ src/app/(temp)/eventos/page.tsx
@@ -1,27 +1,35 @@
 import { eventos } from "@/core";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function PaginaEventos() {
     return (
-        <div className="flex flex-col gap-5">
+        <div className="grid grid-cols-3 gap-5">
             {eventos.map(evento => (
                 <div key={evento.id} className="
-                    flex-col flex-col w-96
+                    flex-col flex-col w-full
                     bg-zinc-800 rounded-lg
                 ">
 
                     <div className="
-                        relative w-96 h-44
+                        relative w-full h-44
                     ">
                         <Image 
                             src={evento.imagem}
                             fill
                             alt={evento.nome}
+                            className="object-cover"
                         />
-
                     </div>
 
-                    <span>{evento.nome}</span>
+                    <div className="flex-1 flex flex-col p-7 gap-5">
+                        <span className="text-lg font-balck">{evento.nome}</span>
+                        <p className="flex-1 text-sm text-zinc-400">{evento.descricao}</p>
+                        <div className="flex gap-5">
+                            <Link className="flex-1 botao vermelho" href={`/evento/admin/${evento.id}/${evento.senha}`}>Admin</Link>
+                            <Link className="flex-1 botao verde" href={`/evento/admin/${evento.id}/${evento.senha}`}>Convite</Link>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
