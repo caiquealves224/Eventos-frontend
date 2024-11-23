c1f29f9f52b4f9213d6c096a8a13ea0a57072d43
caique <caiquealves224@gmail.com>
2024-11-23 08:02:39 -0300

    feat: styling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Pagina from "../components/template/Pagina";
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <Pagina>
-    <div>Inicio</div>
-  </Pagina>
+  <div className="
+    h-screen flex flex-col justify-center items-center gap-10
+    bg-[url('/background-principal.svg')] bg-cover
+  "
+  >
+    <div className="flex flex-col items-center gap-4">
+      {/* <Logo /> */}
+      <p className="text-zinc-500 font-light w-96 leading-6 text-center select-none">
+        Crie e gerencie o convite do seu evento de forma rápido e fácil sem complicações
+      </p>
+    </div>
+    <Link href="/evento" className="botao azul text-lg uppercase">
+      Crie o seu evento
+    </Link>
+  </div>
   );
 }
